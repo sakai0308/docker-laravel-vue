9ed77a4d2a7507f748082a349bb349438cdab4bf
DESKTOP-RQ6H0LN\Hidenori Sakai <hidenori.sakai.0308@gmail.com>
2018-09-01 20:51:47 +0900

    vue-routerを導入。TypeScriptは導入しない。

--- vue/html/ec_web/src/main.js
+++ vue/html/ec_web/src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
-import './styles.scss'
-
-Vue.use(ElementUI)
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
