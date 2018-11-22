bb1bec1a7d42f6a8b6d2eafb0692a74cbe3a9ec5
uezono1052 <uezono1052@gmail.com>
2018-11-23 01:50:37 +0900

    wip headerの基本レイアウト作成

--- vue/html/ec_web/src/App.vue
+++ vue/html/ec_web/src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <div class="header">
+      <h1>VUE.JS EC</h1>
+      <li>ホーム</li>
+      <li>カテゴリー</li>
+      <li>カート</li>
+      <div class="menu">
+        <li></li>
+        <li></li>
+        <li></li>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -11,12 +22,51 @@ export default {
 </script>
 
 <style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+
+li {
+  list-style: none;
+  padding: 0 30px;
+  
+}
+
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+  height: 50px;
+  color: white;
+  background: black;
+  
+}
+
+.menu {
+  width: 40px;
+  height: 40px;
+  margin: 5px 0;
+}
+
+.menu li {
+  color: white;
+  width: 30px;
+  height: 5px;
+  margin: 5px;
+  padding: 0;
+  border-bottom: solid 2px white;
 }
 </style>
