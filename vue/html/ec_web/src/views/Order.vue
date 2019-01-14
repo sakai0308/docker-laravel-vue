dd8f12829dd1f09570318809997d9ef762633fd7
uezono1052 <uezono1052@gmail.com>
2019-01-14 15:43:11 +0900

    wip /order配下のレイアウトを修正

--- vue/html/ec_web/src/views/Order.vue
+++ vue/html/ec_web/src/views/Order.vue
@@ -1,27 +1,25 @@
 <template>
   <el-row>
-    <el-col :span="20" :offset="2">
-      <el-card>
-      <h1>ご購入手続き</h1>
-      <el-col :span="22" :offset="1">
-        <el-form ref="refOrderForm" :model="orderForm" :rules="rulesOrderForm" label-width="160px">
-          <el-form-item label="お名前" prop="name">
-            <el-input v-model="orderForm.name"></el-input>
-          </el-form-item>
-          <el-form-item label="ご住所（都道府県）" prop="prefecture">
-            <el-select v-model="orderForm.prefecture" placeholder="都道府県を選択してください">
-              <el-option label="Zone one" value="shanghai"></el-option>
-              <el-option label="Zone two" value="beijing"></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="ご住所（市区町村）" prop="address">
-            <el-input v-model="orderForm.address"></el-input>
-          </el-form-item>
-          <el-button type="primary" @click="submitForm">購入手続きへ</el-button>
-          <el-button @click="back">キャンセル</el-button>
-        </el-form>
-      </el-col>
-      </el-card>
+    <el-col :gutter="20">
+        <el-col :span="12" :offset="6">
+          <el-form ref="refOrderForm" :model="orderForm" :rules="rulesOrderForm" label-width="160px">
+            <h3>ご購入手続き</h3>
+            <el-form-item label="お名前" prop="name">
+              <el-input v-model="orderForm.name"></el-input>
+            </el-form-item>
+            <el-form-item label="ご住所（都道府県）" prop="prefecture">
+              <el-select v-model="orderForm.prefecture" placeholder="都道府県を選択してください">
+                <el-option label="Zone one" value="shanghai"></el-option>
+                <el-option label="Zone two" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="ご住所（市区町村）" prop="address">
+              <el-input v-model="orderForm.address"></el-input>
+            </el-form-item>
+            <el-button type="info" @click="submitForm">購入手続きへ</el-button>
+            <el-button @click="back">キャンセル</el-button>
+          </el-form>
+        </el-col>
     </el-col>
   </el-row>
 </template>
@@ -64,3 +62,8 @@ export default {
   }
 }
 </script>
+<style>
+ h3 {
+  text-align: center;
+ }
+</style>
