56eba07409bdda06d56b0766389e68e5bf221457
uezono1052 <uezono1052@gmail.com>
2019-01-14 18:42:40 +0900

    注文確定画面のレイアウト修正

--- vue/html/ec_web/src/views/OrderDone.vue
+++ vue/html/ec_web/src/views/OrderDone.vue
@@ -1,15 +1,25 @@
 <template>
-  <el-row>
-    <el-col :span="20" :offset="2">
-      <el-card>
-      <h1>ご注文完了</h1>
-      <div>
-        ご注文番号は {{ orderForm.id }} です。ご利用ありがとうございました。
-      </div>
-      <router-link :to="{ name: 'ItemList' }">
-        商品一覧へ
-      </router-link>
-      </el-card>
+  <el-row class="order_done">
+    <el-col :gutter="24">
+      <el-col :span="12" :offset="6">
+          <el-col>
+            <h3>ご注文が確定しました</h3>
+            <el-col :span="12" :offset="8">
+            <p>下記の注文番号をお控えください。</p>
+            <p>
+              注文番号 :{{ orderForm.id }}
+            </p>
+            <p>ご利用ありがとうございました。</p>
+              <el-col :span="12" :offset="2">
+                <el-button disabled>
+                <router-link class="to_top_btn" :to="{ name: 'ItemList' }">
+                  トップページへ戻る
+                </router-link>
+                </el-button>
+              </el-col>
+              </el-col>
+          </el-col>
+      </el-col>
     </el-col>
   </el-row>
 </template>
@@ -23,3 +33,24 @@ export default {
   }
 }
 </script>
+<style>
+.order_done {
+  vertical-align: middle;
+}
+
+h3 {
+   text-align: center;
+   margin: 0 0 30px;
+  }
+  .to_top_btn {
+  color: black;
+}
+
+p {
+  padding: 10px;
+}
+
+.el-col .el-button {
+  margin-top: 60px;
+}
+</style>
